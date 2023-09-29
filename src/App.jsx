94e9f8281057bb5100898efd5fcbc1f9cbce5246
Aakash Badiyani <badiyani.aakash@gmail.com>
2023-09-29 16:55:05 -0400

    Break form into a component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import { useState } from "react";
 import "./styles.css";
+import TodoForm from "./TodoForm";
 
 export default function App(){
-  const [newItem, setNewItem] = useState("");
+  
   const [todos, setTodos] = useState([]); 
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setTodos((currentTodos)=>{
-      return [...currentTodos, {id: crypto.randomUUID(), title: newItem, completed: false}]
+  const addTodo = (title) => {
+    setTodos((currentTodos) => {
+      return [
+        ...currentTodos,
+        { id: crypto.randomUUID(), title, completed: false },
+      ];
     });
-    setNewItem("");
   }
 
   const deleteTodo = (id) => {
@@ -23,29 +25,19 @@ export default function App(){
     setTodos((currentTodos)=>{
       return currentTodos.map(todo => {
         if(todo.id === id){
-          return {...todo, completed}
+          return {...todo, completed};
         }
-        return todo
+        return todo;
       });
     });
   }
 
   return(
     <>  
-      <form onSubmit={handleSubmit} className="new-item-form">
-        <div className="form-row">
-          <label htmlFor="item">New Item</label>
-          <input 
-            value={newItem} 
-            onChange={e => setNewItem(e.target.value)} 
-            type="text" 
-            id="item">
-          </input>
-        </div>
-        <button className="btn">Add</button>
-      </form>
+      <TodoForm addTodo={addTodo}/>
       <h1 className="header">To do List</h1>
       <ul className="list">
+        {todos.length === 0 && "No Todos"}
         {todos.map((todo)=>{
           return (
           <li key={todo.id}>
